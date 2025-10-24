8e6382798b4719c3cba37f3c12220a8e4504c7ed
Vivek5kwt <vivek5kwt@gmail.com>
2025-10-24 11:41:54 +0530

    Persist and apply locale from authentication response

--- lib/bloc/locale_cubit.dart
+++ lib/bloc/locale_cubit.dart
@@ -1,11 +1,37 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class LocaleCubit extends Cubit<Locale> {
-  LocaleCubit() : super(const Locale('en'));
+import '../utils/session_manager.dart';
 
-  void setLocale(Locale locale) {
+class LocaleCubit extends Cubit<Locale> {
+  LocaleCubit({SessionManager? sessionManager})
+      : _sessionManager = sessionManager ?? const SessionManager(),
+        super(const Locale('en')) {
+    _initialize();
+  }
+
+  final SessionManager _sessionManager;
+
+  Future<void> setLocale(Locale locale) async {
     if (state == locale) return;
     emit(locale);
+    await _sessionManager.savePreferredLanguage(locale.languageCode);
+  }
+
+  void _initialize() {
+    unawaited(_loadSavedLocale());
+  }
+
+  Future<void> _loadSavedLocale() async {
+    final savedLanguage = await _sessionManager.getPreferredLanguage();
+    if (savedLanguage == null || savedLanguage.isEmpty) {
+      return;
+    }
+    if (state.languageCode == savedLanguage) {
+      return;
+    }
+    emit(Locale(savedLanguage));
   }
 }
