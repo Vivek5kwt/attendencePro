166ea44238eb8f902ae10a3a7c7f68604a5e8754
Vivek5kwt <vivek5kwt@gmail.com>
2025-12-03 12:31:07 +0530

    Add AdMob banner to dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:attendancepro/repositories/attendance_repository.dart';
 import 'package:attendancepro/utils/local_notification_service.dart';
 import 'package:attendancepro/widgets/attendance_pro_app.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +12,7 @@ Future<void> main() async {
     markPromptAnswered: false,
   );
   await LocalNotificationService.scheduleDailyAttendanceReminder();
+  await MobileAds.instance.initialize();
 
   final repo = InMemoryAttendanceRepository();
   runApp(AttendanceProApp(repository: repo));
