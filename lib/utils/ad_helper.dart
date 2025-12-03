1ab9e6f9a122ba84f76acfd9c804dd88c8cc5e2f
Vivek5kwt <vivek5kwt@gmail.com>
2025-12-03 16:38:09 +0530

    Set dashboard banner ad unit

--- lib/utils/ad_helper.dart
+++ lib/utils/ad_helper.dart
@@ -5,13 +5,12 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 class AdHelper {
   const AdHelper._();
 
+  // Dashboard banner ad unit provided by product team.
   static const String _androidDashboardBannerAdUnitId =
-      String.fromEnvironment('ANDROID_DASHBOARD_BANNER_AD_UNIT_ID',
-          defaultValue: 'ca-app-pub-2148868058414204/3020458775');
+      'ca-app-pub-2148868058414204/3020458775';
 
   static const String _iosDashboardBannerAdUnitId =
-      String.fromEnvironment('IOS_DASHBOARD_BANNER_AD_UNIT_ID',
-          defaultValue: 'ca-app-pub-2148868058414204/3020458775');
+      'ca-app-pub-2148868058414204/3020458775';
 
   static String get dashboardBannerAdUnitId {
     if (Platform.isAndroid) {
